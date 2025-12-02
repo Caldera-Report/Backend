65067de3467958eecbaac873095a9bcc71dc40e9
ChopstickLeg <72234304+ChopstickLeg@users.noreply.github.com>
2025-12-02 11:30:19 -0500

    #66 Cleaning up indexes (#68)

--- Domain/Migrations/20251202162020_OptimizeIndexes.Designer.cs
+++ Domain/Migrations/20251202162020_OptimizeIndexes.Designer.cs
@@ -3,17 +3,20 @@ using System;
 using Domain.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #nullable disable
 
-namespace Worker.Migrations
+namespace API.Domain.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class WorkerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251202162020_OptimizeIndexes")]
+    partial class OptimizeIndexes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
