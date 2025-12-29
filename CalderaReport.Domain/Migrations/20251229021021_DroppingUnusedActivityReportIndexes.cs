497980d837545fbe81556f1a97eeb32dbbce287e
ChopstickLeg <carpenterhayden1010@gmail.com>
2025-12-28 21:32:09 -0500

    #94 attempting to increase throughput of crawler by removing indexes that have not been used

---
+++ CalderaReport.Domain/Migrations/20251229021021_DroppingUnusedActivityReportIndexes.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class DroppingUnusedActivityReportIndexes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ActivityReports_ActivityId_Date",
+                table: "ActivityReports");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ActivityReports_ActivityId",
+                table: "ActivityReports");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_ActivityReports_ActivityId_Date",
+                table: "ActivityReports",
+                columns: new[] { "ActivityId", "Date" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ActivityReports_ActivityId",
+                table: "ActivityReports",
+                column: "ActivityId");
+        }
+    }
+}
